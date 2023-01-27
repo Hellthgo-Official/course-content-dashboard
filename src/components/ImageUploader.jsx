285229170baa95f9350fae211936b28ccfaed972
inspi-writer <conservedinnovation@gmail.com>
2023-01-27 07:06:02 +0100

    added some functionalities to product upload

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,43 +1,41 @@
-import React, {useCallback} from 'react'
-import { Button, Image } from 'react-bootstrap';
-import { useDropzone } from 'react-dropzone';
-import uploadSvg from './../assets/images/uploader.svg';
+import React, { useCallback } from "react";
+import { Button, Image } from "react-bootstrap";
+import { useDropzone } from "react-dropzone";
+import uploadSvg from "./../assets/images/uploader.svg";
 let binaryStr;
 let files;
 
 function ImageUploader({ open }) {
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
+      const reader = new FileReader();
       files = acceptedFiles.map((file) => (
         <li key={file.path}>
           {file.path} - {file.size} bytes
         </li>
       ));
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-      // Do whatever you want with the file contents
-        binaryStr = reader.result
-        console.log(binaryStr)
-      }
-      reader.readAsArrayBuffer(file)
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
-  
-  
-  console.log(binaryStr)
+        // Do whatever you want with the file contents
+        binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  // console.log(binaryStr);
 
   return (
     <div className="container">
-      <div {...getRootProps({ className: 'dropzone' })}>
+      <div {...getRootProps({ className: "dropzone" })}>
         <input {...getInputProps()} />
         <Image src={uploadSvg} />
       </div>
       <aside>
-        <ul className='mt-5'>{files}</ul>
+        <ul className="mt-5">{files}</ul>
       </aside>
     </div>
   );
--- src/screens/ProductUpload.jsx
+++ src/screens/ProductUpload.jsx
@@ -5,13 +5,20 @@ import {
   Container,
   Form,
   Image,
-  Row,
-} from 'react-bootstrap';
-import productUpload from '../assets/images/product-upload.svg';
-import BootstrapSwitchButton from 'bootstrap-switch-button-react';
-import ImageUploader from '../components/ImageUploader';
+  Row
+} from "react-bootstrap";
+import productUpload from "../assets/images/product-upload.svg";
+import BootstrapSwitchButton from "bootstrap-switch-button-react";
+import ImageUploader from "../components/ImageUploader";
+import { useState } from "react";
 
 function ProductUpload() {
+  const [productName, setProductName] = useState();
+  const [price, setPrice] = useState();
+  const [productDescription, setProductDescription] = useState();
+  const [productUri, setProductUri] = useState();
+  const [productImages, setProductImages] = useState([]);
+
   return (
     <Container className="">
       <div className="text-primary my-5 d-flex ">
@@ -19,7 +26,7 @@ function ProductUpload() {
           width={50}
           src={productUpload}
           className=""
-          style={{ margin: '0 20px 10px 0' }}
+          style={{ margin: "0 20px 10px 0" }}
         />
         <h3 className="text-primary">Product Upload</h3>
       </div>
@@ -30,37 +37,73 @@ function ProductUpload() {
               <div className="row d-flex ">
                 <Form.Group className="mb-3 col-6" controlId="formBasicEmail">
                   <Form.Label>Product Name</Form.Label>
-                  <Form.Control type="text" placeholder="" className="border-default bg-primary" />
+                  <Form.Control
+                    type="text"
+                    placeholder=""
+                    className="border-default bg-primary"
+                    onChange={(e) => {
+                      setProductName(e.target.value);
+                    }}
+                  />
                 </Form.Group>
-                <Form.Group className="mb-3 col-3" style={{marginTop:"30px"}} controlId="formBasicEmail">
-                 
-                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-3"
+                  style={{ marginTop: "30px" }}
+                  controlId="formBasicEmail"
+                ></Form.Group>
                 <Form.Group className="mb-3 col-3" controlId="formBasicEmail">
                   <Form.Label className="text-end">Price in #</Form.Label>
-                  <Form.Control type="text" placeholder="" className="border-default bg-primary"/>
+                  <Form.Control
+                    type="text"
+                    placeholder=""
+                    className="border-default bg-primary"
+                    onChange={(e) => {
+                      setPrice(e.target.value);
+                    }}
+                  />
                 </Form.Group>
               </div>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Product Description</Form.Label>
-                <Form.Control as="textarea" rows={3} className="border-default bg-primary"/>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  className="border-default bg-primary"
+                  onChange={(e) => {
+                    setProductDescription(e.target.value);
+                  }}
+                />
               </Form.Group>
 
               <div className="row d-flex ">
-              <Form.Group className="mb-3 col-6" controlId="formBasicEmail">
-                <Form.Label>Product Images</Form.Label>
-                <div className="bg-white p-3">
-                  <ImageUploader />
-                </div>
-              </Form.Group>
-              <Form.Group className="mb-3 col-6" controlId="formBasicEmail">
-                <Form.Label>Product URI</Form.Label>
-                <Form.Control type="text" placeholder="" className="border-default bg-primary" />
-              </Form.Group>
+                <Form.Group className="mb-3 col-6" controlId="formBasicEmail">
+                  <Form.Label>Product Images</Form.Label>
+                  <div className="bg-white p-3">
+                    <ImageUploader />
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3 col-6" controlId="formBasicEmail">
+                  <Form.Label>Product URI</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder=""
+                    className="border-default bg-primary"
+                    onChange={(e) => {
+                      setProductUri(e.target.value);
+                    }}
+                  />
+                </Form.Group>
               </div>
 
               <div className="mt-4">
-                <Button size="lg" className="border-default" >
+                <Button
+                  size="lg"
+                  className="border-default"
+                  onClick={() =>
+                    console.log([productName, price, productDescription])
+                  }
+                >
                   Upload Product
                 </Button>
               </div>
